7914dad5f129a92efd6a9c4fd6932648f289b07c
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-26 03:02:17 +0300

    Commit 6 | AppSettings and Services Updated

--- UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Program.cs
@@ -1,3 +1,7 @@
+using FreeCourse.Services.Catalog.Settings;
+using Microsoft.Extensions.Options;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,6 +11,13 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
+
+builder.Services.AddSingleton<IDatabaseSettings>(sp =>
+{
+    return sp.GetRequiredService<IOptions<IDatabaseSettings>>().Value;
+});
 
 var app = builder.Build();
 
