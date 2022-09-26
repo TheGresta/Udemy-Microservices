e9d1fd7d605992a3482113423f0107d33d4267db
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-09-26 12:34:06 +0300

    Commit 9 | Controllers Created

---
+++ UdemyMicroservices/Services/Catalog/FreeCourse.Services.Catalog/Controllers/CoursesController.cs
@@ -0,0 +1,67 @@
+﻿using FreeCourse.Services.Catalog.Dtos;
+using FreeCourse.Services.Catalog.Services;
+using FreeCourse.Shared.BaseControllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoursesController : CustomBaseController
+    {
+        private readonly ICourseService _courseService;
+
+        public CoursesController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            var response = await _courseService.GetAsync();
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("GetById/{id}")]
+        public async Task<IActionResult> GetById(string id)
+        {
+            var response = await _courseService.GetByIdAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpGet("GetByUserId/{userId}")]
+        public async Task<IActionResult> GetAllByUserId(string userId)
+        {
+            var response = await _courseService.GetAllByUSerIdAsync(userId);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPost("Create")]
+        public async Task<IActionResult> Create(CourseCreateDto courseCreateDto)
+        {
+            var response = await _courseService.CreateAsync(courseCreateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(CourseUpdateDto courseUpdateDto)
+        {
+            var response = await _courseService.UpdateAsync(courseUpdateDto);
+
+            return CreateActionResultInstance(response);
+        }
+
+        [HttpDelete("Delete/{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var response = await _courseService.DeleteAsync(id);
+
+            return CreateActionResultInstance(response);
+        }
+    }
+}
---
+++ UdemyMicroservices/Shared/FreeCourse.Shared/BaseControllers/CustomBaseController.cs
@@ -0,0 +1,16 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Shared.BaseControllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode
+            };
+        }
+    }
+}
